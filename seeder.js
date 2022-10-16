cf883c515b9142f503b6099347f950abe3272b03
reghay-repo <osm2014@hotmail.fr>
2022-10-16 21:03:44 +0100

    make seeder file

---
+++ seeder.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+const fs = require('fs');
+
+// load models
+const Bootcamp = require('./models/Bootcamp');
+
+// load environement variables
+require('dotenv').config({path:'./config/config.env'});
+
+
+
+// connect to mongodb 
+mongoose.connect(process.env.MONGO_URI);
+
+// load data form file
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+
+
+// import to DB
+const importToDB = async () => {
+    try {
+        await Bootcamp.insertMany(bootcamps);
+        console.log('Data imported succesfully!'.green.inverse);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+// import to DB
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany({});
+        console.log('Data deleted succesfully!'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.err(err)
+    }
+}
+
+
+
+if(process.argv[2] === '-i') {
+    importToDB();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
